2638cfde45abc6ce7ab79a3ef8eb78170e1f7097
ilover <admin@ilover.me>
2021-01-28 17:32:59 +0800

    紧急修复小bug

--- src/core-components/window/src/window-content.ts
+++ src/core-components/window/src/window-content.ts
@@ -19,7 +19,7 @@ export default {
             style: {
                 flex: 1,
                 userSelect: this.instance.isActive ? 'text' : 'none',
-                // overflow: 'auto',
+                overflow: 'auto',
                 position: 'relative'
             }
         }, h(this.instance.component, { instance: this.instance }))
